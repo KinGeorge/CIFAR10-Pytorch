ddb2c652eaa2b4fbf6ee0239b62d9ae6e3bdc0eb
unknown <2440700833@qq.com>
2022-05-22 17:17:17 +0800

    rm

--- scripts/exe_res.sh
+++ scripts/exe_res.sh
@@ -1,3 +1,3 @@
 echo "==========CIFAR10 With CNN=========="
-python main.py --mode 6 --num_epochs 35 --lr 0.00001
+python main.py --mode 5 --num_epochs 35 --lr 0.00001
 echo "==========Training Complete=========="
